42b439f23e6d714a46ada33348999f68154d9227
Amir Eldor <amir@eldor.dev>
2022-05-17 00:43:04 +0300

    Add some stuff to custom build based on sprite minimal loader

--- phaser-custom.js
+++ phaser-custom.js
@@ -4,26 +4,43 @@ var CONST = require('const');
 var Extend = require('utils/object/Extend');
 
 var Phaser = {
+  Cameras: {
+    Scene2D: require('cameras/2d'),
+  },
+  Events: require('events/index'),
+  Game: require('core/Game'),
+  GameObjects: {
+    DisplayList: require('gameobjects/DisplayList'),
+    UpdateList: require('gameobjects/UpdateList'),
 
-   Cameras: {
-       Scene2D: require('cameras/2d')
-   },
-   Events: require('events/index'),
-   Game: require('core/Game'),
-   GameObjects: {
-       DisplayList: require('gameobjects/DisplayList'),
-       UpdateList: require('gameobjects/UpdateList'),
-       Graphics: require('gameobjects/graphics/Graphics.js'),
-       Factories: {
-           Graphics: require('gameobjects/graphics/GraphicsFactory')
-       },
-       Creators: {
-           Graphics: require('gameobjects/graphics/GraphicsCreator')
-       }
-   },
-   Math: {
-        Between: require('math/Between')
-   }
+    Image: require('gameobjects/image/Image'),
+    Sprite: require('gameobjects/sprite/Sprite'),
+
+    Factories: {
+      Image: require('gameobjects/image/ImageFactory'),
+      Sprite: require('gameobjects/sprite/SpriteFactory'),
+      Text: require('gameobjects/text/TextFactory'),
+    },
+
+    Creators: {
+      Image: require('gameobjects/image/ImageCreator'),
+      Sprite: require('gameobjects/sprite/SpriteCreator'),
+    },
+  },
+  Loader: {
+    FileTypes: {
+      ImageFile: require('loader/filetypes/ImageFile'),
+    },
+    LoaderPlugin: require('loader/LoaderPlugin'),
+  },
+  Math: {
+    Between: require('math/Between'),
+  },
+  Scale: require('scale'),
+  ScaleModes: require('renderer/ScaleModes'),
+  Scene: require('scene/Scene'),
+  Scenes: require('scene'),
+  Text: require('gameobjects/text/Text'),
 };
 
 //  Merge in the consts
